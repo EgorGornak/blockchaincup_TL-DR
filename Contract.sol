00c4ce7976b004d06eea1a965bc860ff67016690
yosabaki <32043657+yosabaki@users.noreply.github.com>
2018-04-22 14:58:48 +0300

    fix codestyle and add comments

--- Contract.sol
+++ Contract.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.16;
 contract owned {
     address public owner;
 
-    function owned() public {
+    constructor() public {
         owner = msg.sender;
     }
 
@@ -19,15 +19,15 @@ contract owned {
 
 contract TokenWinx is owned {
     mapping (address => uint) public balances;
-    function give(uint amount) {
+    function give(uint amount) public {
         balances[msg.sender] += amount;
     }
-    function send(address receiver, uint amount) {
+    function send(address receiver, uint amount) public {
         if(balances[msg.sender] < amount) return;
         balances[msg.sender] -= amount;
         balances[receiver] += amount;
     }
-    function balanceOf(address addr) constant returns (uint balance) {
+    function balanceOf(address addr) public constant returns (uint balance) {
         balance = balances[addr];
     }
 }
@@ -43,19 +43,19 @@ contract tokenVote is owned {
 
     mapping (address => address) public voters; // map of registered voters in system
     mapping (address => Expert) public experts; // map of registered experts in system
-    mapping (address => bool) public isExpert; // checking map that current user is registered as expert
-    mapping (address => bool) public isVoter; // checking map that current user is registered as voter
+    mapping (address => bool) public isExpert; // checking map that current account is registered as expert
+    mapping (address => bool) public isVoter; // checking map that current account is registered as voter
     mapping (address => uint32) public expertsIndexes; // auxiliary map of indexes in expertsArray
     address[] public expertsArray; // array of expert addresses 
     mapping (address => uint32) public projects; // map of projects registered in system
     address[] public votersArray; // array of voter addresses
     mapping (address => uint32) public votersIndexes; // auxiliary map of indexes in votersArray
-    mapping (address => bool) public isProject;
-    mapping (address => uint32) public projectsIndexes;
-    address[] public projectsArray;
-    uint32[] public projectsVotes;
+    mapping (address => bool) public isProject; //checking map that  current account is registered as project
+    mapping (address => uint32) public projectsIndexes; // auxiliary map of indexes in projectsArray
+    address[] public projectsArray; // addresses of registered Projects
+    uint32[] public projectsVotes;  // number of votes for current project
     
-    function tokenVote(address _TokenWinx_address)  {
+    constructor(address _TokenWinx_address) public {
         TokenWinx_address = _TokenWinx_address;
     }
     
@@ -64,21 +64,22 @@ contract tokenVote is owned {
         uint32 usedVotes;
     }
     
-    function vote (address project, uint32 count) {
+    function vote (address project, uint32 count) public {
         require(currStep == 2); // experts can vote for projects only on the second step
-        require(isProject[project]);
+        require(isProject[project]); // check that project is registered
         require(isExpert[msg.sender]); // check that sender is expert
         require(experts[msg.sender].votes - experts[msg.sender].usedVotes >= count); // check that expert have enough unused votes
-        experts[msg.sender].usedVotes+=count; // add appropriate number of votes to project
+        experts[msg.sender].usedVotes += count; // add appropriate number of votes to project
         projects[project] += count;
         projectsVotes[projectsIndexes[project]] += count;
     }
     
-    function setProjects(address[] newProjects) public {
-        require(currStep == 0);
+    function setProjects(address[] newProjects) public { // Set projects array to current argument array of addresses
+        require(currStep == 0); // Owner can set projects only when voting isn`t proccessing
         require(msg.sender == owner);
         uint32 i = 0;
-        while (i < projectsArray.length) {
+        
+        while (i < projectsArray.length) { // delete current projects arrays and maps
             isProject[projectsArray[i]] = false;
             projects[projectsArray[i]] = 0;
             i++;
@@ -88,7 +89,7 @@ contract tokenVote is owned {
             delete projectsVotes[projectsVotes.length - 1];
         }
         i = 0;
-        while (i < newProjects.length) {
+        while (i < newProjects.length) { // set project arrays and map to argument of function
             projectsArray.push(newProjects[i]);
             isProject[newProjects[i]] = true;
             projectsIndexes[newProjects[i]] = i;
@@ -97,7 +98,7 @@ contract tokenVote is owned {
         }
     }
     
-    function giveVote (address expert) {
+    function giveVote (address expert) public { // set Voter pointer to expert
         require(currStep == 1); // voters can give their votes before second step of voting 
         TokenWinx _TokenWinx = TokenWinx(TokenWinx_address);
         if (_TokenWinx.balanceOf(msg.sender) == 0) { // check that sender is owner of Winx tokens
@@ -106,9 +107,9 @@ contract tokenVote is owned {
                 return;
             }
         }
-        
+        require(!isExpert[msg.sender]);
         if (!isVoter[msg.sender]) { // if voter isn`t registered then register him
-            addVoter(msg.sender,expert);
+            addVoter(msg.sender, expert);
         } else if (isExpert[voters[msg.sender]]) {
             experts[voters[msg.sender]].votes--;
         }
@@ -116,21 +117,26 @@ contract tokenVote is owned {
         experts[expert].votes++;
     }
     
-    function addExpert(address user) {
+    function addExpert(address user) public { // owner can register new expert
         TokenWinx _TokenWinx = TokenWinx(TokenWinx_address);
         require(currStep == 0); // Requires that voting is stopeed to avoid problems
         require(msg.sender == owner); // Only administrator can add or delete experts
-        require(_TokenWinx.balanceOf(user)>0); // Can add only user with token Winx
+        require(_TokenWinx.balanceOf(user) > 0); // Can add only user with token Winx
         require(!isExpert[user]); // Requires that expert isn`t registered;
+        if (isVoter[user]) {
+            deleteVoter(user);
+        }
         isExpert[user] = true;
         experts[user] = Expert(1,0);
         expertsArray.push(user);
         expertsIndexes[user] = uint32(expertsArray.length) - 1;
     }
     
-    function deleteVoter(address tmp) private {
+    function deleteVoter(address tmp) private { // auxiliary function, that deletes voter from system
         isVoter[tmp] = false;
-        experts[voters[tmp]].votes--;
+        if (isExpert[voters[tmp]]) {
+            experts[voters[tmp]].votes--;
+        }
         delete voters[tmp];
         uint32 i = votersIndexes[tmp];
         delete votersIndexes[tmp];
@@ -139,14 +145,14 @@ contract tokenVote is owned {
         delete votersArray[votersArray.length - 1];
     }
     
-    function addVoter(address user,address expert) private {
+    function addVoter(address user,address expert) private { // auxiliary functionn, that register new voter with pointer to expert
         isVoter[user] = true;
         voters[user] = expert;
         votersArray.push(user);
         votersIndexes[user] = uint32(votersArray.length) - 1;
     }
     
-    function deleteExpert(address user) {
+    function deleteExpert(address user) public {  // Owner can delete expert from system
         require(currStep == 0); // Requires that voting is stopeed to avoid problems
         require(msg.sender == owner); 
         require(isExpert[user]); // Allowed to delete only registered experts
@@ -159,18 +165,17 @@ contract tokenVote is owned {
         delete expertsArray[expertsArray.length - 1];
     }
     
-    function checkVoters() private {
-        require(currStep == 2);
+    function checkVoters() private { // check that voters are valid
         TokenWinx _TokenWinx = TokenWinx(TokenWinx_address);
         for (uint32 i = 0; i < votersArray.length; i++) {
             address tmp = votersArray[i];
-            if (_TokenWinx.balanceOf(tmp) == 0) {
+            if (_TokenWinx.balanceOf(tmp) == 0) { // if balance of current voter is 0 then he isn`t valid
                 deleteVoter(tmp);
             } 
         }
     }
     
-    function nextStep() {
+    function nextStep() public { // go to the next step of voting
         require (msg.sender == owner); // only owner can manipulate with voting steps
         require (currStep < 2); // check that current step isn`t terminal
         currStep++;
@@ -179,11 +184,11 @@ contract tokenVote is owned {
         }
     }
     
-    function stopVoting() {
+    function stopVoting() public { // owner stops voting
         require(msg.sender == owner);
         require(currStep == 2); // check that current step is terminal
         currStep = 0; // stop voting
-        for (uint256 i = 0; i< expertsArray.length; i++) { // set nulls to all experts
+        for (uint256 i = 0; i < expertsArray.length; i++) { // set nulls to all experts
             experts[expertsArray[i]].usedVotes = 0;
         }
     }
